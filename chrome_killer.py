427f17f2defc92590710bca42a2a224dc874a1bc
eugene <eugene@eugene-H61M-USB3-B3.(none)>
2012-04-24 16:30:00 +0700

    impr

--- chrome_killer.py
+++ chrome_killer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+import logging
+LOG_FILENAME = '/home/eugene/vzabote-xinetd.log'
+logging.basicConfig(filename=LOG_FILENAME,
+     level=logging.ERROR,
+     )
 
 
 def kill_chrome(file_path, elapsed_time, command_to_execute, command_arg):
@@ -9,11 +14,14 @@ def kill_chrome(file_path, elapsed_time, command_to_execute, command_arg):
     
     if t2 - t1 > elapsed_time:
         from subprocess import call
+        _t = time.strftime("%a, %d %b %Y %H:%M:%S +06", time.gmtime())
+        logging.error('%s long delay. restart browser' % _t)
         call([command_to_execute, command_arg])
+
         
 if __name__ == '__main__':
     file_path = '/home/eugene/t'
-    elapsed_time = 25
+    elapsed_time = 2
     command_to_execute = 'touch'
     command_arg = '/home/eugene/t2'
     kill_chrome(file_path, elapsed_time, command_to_execute, command_arg)
